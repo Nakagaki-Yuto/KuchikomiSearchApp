a67f6d6768963e5f81aea18d7e6ae947541b031c
Nakagaki-Yuto <yuto.nakagaki@gmail.com>
2020-10-22 01:55:52 +0900

    add files for Heroku

--- kuchikomisite/settings.py
+++ kuchikomisite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$0sg%1vr1qp4xj$d@z&%4$v(md4ce+z4u+_+l#67w@y0s#gwd7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
@@ -92,6 +93,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -141,3 +146,7 @@ LOGIN_REDIRECT_URL = '/'
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1056172925919-dooa6i7e3dhhn4q8rq2aa0rpgg06qpaa.apps.googleusercontent.com' # クライアントID
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'xiZ7vj8YdKp3mPaDVp41x18D'  # クライアント シークレット
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
